1be6d37bec8a1f3fb832e8c4178e782ddb9cc307
Lucas Marianno <lucas.marianno94@gmail.com>
2024-07-05 01:17:08 -0300

    drag target area refactored

--- lib/main.dart
+++ lib/main.dart
@@ -115,34 +115,41 @@ class KanbanColumn extends StatelessWidget {
     return Container(
       width: width,
       margin: const EdgeInsets.symmetric(horizontal: 3),
+      padding: const EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        color: Colors.blueGrey[100],
+        borderRadius: BorderRadius.circular(10),
+      ),
       child: Column(
         children: [
           Center(child: Text(columnName)),
-          DragTarget(
-            onAcceptWithDetails: (data) {
-              if (data.data is Task) {
-                print(data.offset);
-                taskList?.add(data.data as Task);
-              }
-            },
-            builder: (context, candidateData, rejectedData) {
-              return ListView.builder(
-                shrinkWrap: true,
-                itemCount: max(taskList?.length ?? 0, 1),
-                padding: const EdgeInsets.symmetric(vertical: 1),
-                itemBuilder: (context, index) {
-                  if (taskList == null || taskList!.isEmpty) {
-                    return const Center(child: Text('No tasks here!'));
-                  }
+          Expanded(
+            child: DragTarget(
+              onAcceptWithDetails: (data) {
+                if (data.data is Task) {
+                  print(data.offset);
+                  taskList?.add(data.data as Task);
+                }
+              },
+              builder: (context, candidateData, rejectedData) {
+                return ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: max(taskList?.length ?? 0, 1),
+                  padding: const EdgeInsets.symmetric(vertical: 1),
+                  itemBuilder: (context, index) {
+                    if (taskList == null || taskList!.isEmpty) {
+                      return const Center(child: Text('No tasks here!'));
+                    }
 
-                  return KanbanTile(
-                    task: taskList![index],
-                    tileHeight: width / 3,
-                    tileWidth: width,
-                  );
-                },
-              );
-            },
+                    return KanbanTile(
+                      task: taskList![index],
+                      tileHeight: width / 3,
+                      tileWidth: width,
+                    );
+                  },
+                );
+              },
+            ),
           ),
         ],
       ),
@@ -189,7 +196,7 @@ class KanbanTile extends StatelessWidget {
           margin: const EdgeInsets.symmetric(vertical: 3),
           padding: const EdgeInsets.all(2),
           decoration: BoxDecoration(
-            color: Colors.blueGrey[100],
+            color: Colors.blueGrey[300],
             borderRadius: BorderRadius.circular(10),
           ),
           child: Column(
