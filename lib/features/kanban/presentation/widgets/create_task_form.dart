3968c7923a55470bd6aea8bd4590e701425e0ac1
Lucas Marianno <lucas.marianno94@gmail.com>
2024-07-11 16:32:29 -0300

    BUGFIX: create_task_form updated to properly copy a task without editing its instance

--- lib/features/kanban/presentation/widgets/create_task_form.dart
+++ lib/features/kanban/presentation/widgets/create_task_form.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:kanban/core/constants/enum/assignee.dart';
-import 'package:kanban/core/constants/enum/task_importance.dart';
 import 'package:kanban/core/constants/enum/task_status.dart';
 import 'package:kanban/core/widgets/form_widgets/form_date_picker.dart';
 import 'package:kanban/core/widgets/form_widgets/form_drop_down_menu_button.dart';
@@ -10,25 +9,22 @@ import 'package:kanban/core/widgets/form_widgets/form_field.dart';
 import 'package:kanban/features/kanban/domain/entities/task_entity.dart';
 
 class TaskFromModalBottomForm {
-  static final Task _newTask = Task(
-    title: 'Nova Tarefa',
-    status: TaskStatus.todo,
-    taskImportance: TaskImportance.normal,
-    dueDate: null,
-    description: null,
-    createdDate: Timestamp.now(),
-    assingnedTo: Assignee.myself,
-  );
+  static final Task _newTask = Task(title: 'Nova Tarefa');
 
   static Future<Task?> newTask(BuildContext context) async {
-    return await editTask(_newTask, context);
+    return await showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return _EditTaskForm(_newTask, formTitle: 'Adicionar Tarefa');
+      },
+    );
   }
 
   static Future<Task?> editTask(Task task, BuildContext context) async {
     return await showModalBottomSheet(
       context: context,
       builder: (context) {
-        return _EditTaskForm(task);
+        return _EditTaskForm(task, formTitle: 'Editar Tarefa');
       },
     );
   }
@@ -36,7 +32,8 @@ class TaskFromModalBottomForm {
 
 class _EditTaskForm extends StatefulWidget {
   final Task task;
-  const _EditTaskForm(this.task);
+  final String formTitle;
+  const _EditTaskForm(this.task, {required this.formTitle});
 
   @override
   State<_EditTaskForm> createState() => _EditTaskFormState();
@@ -48,13 +45,13 @@ class _EditTaskFormState extends State<_EditTaskForm> {
   void sendForm() {
     if (newTask.title == '') return;
 
-    newTask.createdDate = Timestamp.now();
+    newTask.createdDate ??= Timestamp.now();
     Navigator.pop(context, newTask);
   }
 
   @override
   void initState() {
-    newTask = widget.task;
+    newTask = widget.task.copy();
     super.initState();
   }
 
@@ -74,70 +71,70 @@ class _EditTaskFormState extends State<_EditTaskForm> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           FormTitle(
-            title: 'Adicionar Tarefa (Detalhada)',
+            title: widget.formTitle,
             onIconPressed: sendForm,
             icon: Icons.add,
           ),
           const Divider(),
           Expanded(
-            child: Form(
-              child: ListView(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 5),
-                    child: Text(
-                      "* campos obrigatórios!",
-                      textAlign: TextAlign.end,
+            child: ListView(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5),
+                  child: Text(
+                    "* campos obrigatórios!",
+                    textAlign: TextAlign.end,
+                  ),
+                ),
+                MyFormField(
+                  label: 'Tarefa',
+                  initialValue: newTask.title,
+                  onChanged: (newString) {
+                    newTask.title = newString!;
+                  },
+                  mandatory: true,
+                ),
+                MyFormField(
+                  label: 'Descrição da tarefa',
+                  initialValue: newTask.description,
+                  multilineFormField: true,
+                  onChanged: (newString) {
+                    newTask.description = newString;
+                  },
+                ),
+                FormDropDownMenuButton(
+                  label: 'Responsável pela tarefa',
+                  initialValue: newTask.assingnedTo.name,
+                  items: Assignee.values.map((e) => e.name).toList(),
+                  onChanged: (newValue) {
+                    newTask.assingnedTo = Assignee.fromString(newValue);
+                  },
+                ),
+                Row(
+                  children: [
+                    FormDropDownMenuButton(
+                      maxWidth: width / 2 - 3,
+                      label: 'Status da tarefa',
+                      initialValue: newTask.status.name,
+                      items: TaskStatus.values.map((e) => e.name).toList(),
+                      onChanged: (newValue) {
+                        newTask.status = TaskStatus.fromString(newValue);
+                      },
                     ),
-                  ),
-                  MyFormField(
-                    label: 'Tarefa',
-                    initialValue: newTask.title,
-                    onChanged: (newString) {
-                      newTask.title = newString;
-                    },
-                    mandatory: true,
-                  ),
-                  MyFormField(
-                    label: 'Descrição da tarefa',
-                    initialValue: newTask.description,
-                    multilineFormField: true,
-                    onChanged: (newString) {
-                      newTask.description = newString;
-                    },
-                  ),
-                  FormDropDownMenuButton(
-                    label: 'Responsável pela tarefa',
-                    initialValue: newTask.assingnedTo.name,
-                    items: Assignee.values.map((e) => e.name).toList(),
-                    onChanged: (newValue) {
-                      newTask.assingnedTo = Assignee.fromString(newValue);
-                    },
-                  ),
-                  Row(
-                    children: [
-                      FormDropDownMenuButton(
-                        maxWidth: width / 2 - 3,
-                        label: 'Status da tarefa',
-                        initialValue: newTask.status.name,
-                        items: TaskStatus.values.map((e) => e.name).toList(),
-                        onChanged: (newValue) {
-                          newTask.status = TaskStatus.fromString(newValue);
-                        },
-                      ),
-                      const SizedBox(width: 6),
-                      FormDatePicker(
-                        label: 'Prazo da tarefa',
-                        initialDate: newTask.dueDate?.toDate(),
-                        maxWidth: width / 2 - 3,
-                        onChanged: (newDate) {
-                          newTask.dueDate = Timestamp.fromDate(newDate);
-                        },
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                    const SizedBox(width: 6),
+                    FormDatePicker(
+                      label: 'Prazo da tarefa',
+                      initialDate: newTask.dueDate?.toDate(),
+                      maxWidth: width / 2 - 3,
+                      onChanged: (newDate) {
+                        newTask.dueDate = newDate == null
+                            ? null
+                            : Timestamp.fromDate(newDate);
+                      },
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ],
