632a3c295a5a29d3023931163e2712788b72e158
Lucas Marianno <lucas.marianno94@gmail.com>
2024-07-11 16:33:20 -0300

    FEATURE: kanban_tile: now has edit and delete options

--- lib/features/kanban/presentation/widgets/kanban_tile.dart
+++ lib/features/kanban/presentation/widgets/kanban_tile.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:kanban/features/kanban/data/remote/firestore_service.dart';
-import 'package:kanban/features/kanban/presentation/widgets/create_task_form.dart';
-
+import 'package:kanban/features/kanban/domain/repository/task_repository.dart';
 import '../../domain/entities/task_entity.dart';
-import '../../domain/repository/task_repository.dart';
 
 class KanbanTile extends StatelessWidget {
   final Task task;
@@ -24,7 +21,7 @@ class KanbanTile extends StatelessWidget {
       data: task,
       onDragEnd: (details) {
         if (details.wasAccepted) {
-          removeTaskFromColumn(task);
+          // TODO: remove task from status column
         }
       },
       feedback: tile,
@@ -37,53 +34,61 @@ class KanbanTile extends StatelessWidget {
     );
   }
 
-  Widget _createTile(BuildContext context) => Material(
-        color: Colors.transparent,
-        child: Container(
-          height: tileHeight,
-          width: tileWidth,
-          margin: const EdgeInsets.symmetric(vertical: 3),
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-          decoration: BoxDecoration(
-            color: Colors.blueGrey[300],
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              // tarefa proposta
-              ListTile(
-                title: Text(
-                  task.title,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                trailing: IconButton(
-                  onPressed: () async {
-                    // TODO: display task options (edit | delete)
-                    final response =
-                        await TaskFromModalBottomForm.editTask(task, context);
-                    if (response != null) {
-                      // Call firestore to edit
-                      FirestoreService.editTask(response);
-                    }
-                  },
-                  icon: const Icon(Icons.more_horiz),
-                ),
-              ),
-              // Permitir que o usuário escolha um icone de uma lista
-              const Row(
-                children: [
-                  Icon(Icons.tapas_outlined),
-                  // Exibir a foto da pessoa que foi atribuida a tarefa
-                  Icon(Icons.person),
-                  SizedBox(width: 10),
-                  // Permitir atribuir um nível de importancia para a tarefa
-                  Icon(Icons.label_important),
-                ],
-              ),
-            ],
-          ),
+  Widget _createTile(BuildContext context) {
+    TaskRepository taskRepo = TaskRepository(context);
+
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        height: tileHeight,
+        width: tileWidth,
+        margin: const EdgeInsets.symmetric(vertical: 3),
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        decoration: BoxDecoration(
+          color: Colors.blueGrey[300],
+          borderRadius: BorderRadius.circular(10),
         ),
-      );
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // tarefa proposta
+            ListTile(
+              title: Text(
+                task.title,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+              // show edit / delete task
+              trailing: PopupMenuButton(
+                itemBuilder: (context) {
+                  print(task.toJson());
+                  return [
+                    PopupMenuItem(
+                      child: const Text('Edit'),
+                      onTap: () => taskRepo.editTask(task),
+                    ),
+                    PopupMenuItem(
+                      child: const Text('Delete'),
+                      onTap: () => taskRepo.deleteTask(task),
+                    ),
+                  ];
+                },
+              ),
+            ),
+            // Permitir que o usuário escolha um icone de uma lista
+            const Row(
+              children: [
+                Icon(Icons.tapas_outlined),
+                // Exibir a foto da pessoa que foi atribuida a tarefa
+                Icon(Icons.person),
+                SizedBox(width: 10),
+                // Permitir atribuir um nível de importancia para a tarefa
+                Icon(Icons.label_important),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
